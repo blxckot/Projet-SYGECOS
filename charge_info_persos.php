4a8ea337a1eb98347c649c0b9d3dd5e1e5fa3f6c
Xoxo lnd <axellelogbo04@gmail.com>
2025-07-01 02:48:16 +0000

    test commit

--- charge_info_persos.php
+++ charge_info_persos.php
@@ -15,7 +15,7 @@ if (!isLoggedIn()) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SYGECOS - Informations personnelles</title>
+    <title>SYGECOS - Informations ersonnelles</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <style>
         :root {
